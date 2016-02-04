ac439875327655330a209d3cd1ba544c505191b1
Guillaume Miranda <guillaume11miranda@gmail.com>
2016-02-04 12:20:04 +0100

    Added Item bundle

---
+++ src/ItemBundle/Entity/Item.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace ItemBundle\Entity;
+
+/**
+ * Item
+ */
+class Item
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var int
+     */
+    private $price;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Item
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set price
+     *
+     * @param integer $price
+     *
+     * @return Item
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return int
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Item
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
+
---
+++ src/ItemBundle/ItemBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ItemBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ItemBundle extends Bundle
+{
+}
