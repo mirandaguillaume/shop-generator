a7b3982d9fee58b68ae95b94202e4174faf897ca
Guillaume Miranda <guillaume11miranda@gmail.com>
2016-03-08 11:09:34 +0100

    Added CracterUtilities service

---
+++ src/CharacterBundle/Services/CharacterUtilities.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guillaume
+ * Date: 07/03/16
+ * Time: 19:47
+ */
+
+namespace CharacterBundle\Services;
+
+
+class CharacterUtilities
+{
+
+    public function lowerStat($stat, $level){
+
+    }
+
+}
